d934e9b14213be83dadb677ababf85614b3ae97c
Ilyas Nur Rohman <ilyasnurrohman14@gmail.com>
2025-07-10 18:30:10 +0700

    commit pertama

---
+++ pemesanan.js
@@ -0,0 +1,32 @@
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        event.preventDefault(); 
+
+        if (!form.checkValidity()) {
+          event.stopPropagation();
+          form.classList.add("was-validated");
+          return; 
+        }
+
+        // SweetAlert
+        Swal.fire({
+          title: "Success!",
+          text: "Pesanan anda telah diterima. Check email anda untuk melanjutkan pembayaran.",
+          icon: "success",
+          confirmButtonText: "OK",
+        }).then(() => {
+          form.reset();
+          form.classList.remove("was-validated");
+        });
+      },
+      false
+    );
+  });
+})();
