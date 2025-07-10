4c338fa60e5470ddc6584697f8de3b1d5b373895
Ilyas Nur Rohman <ilyasnurrohman14@gmail.com>
2025-07-10 18:44:48 +0700

    commit kedua

--- pemesanan.js
+++ pemesanan.js
@@ -7,12 +7,12 @@
     form.addEventListener(
       "submit",
       (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
 
         if (!form.checkValidity()) {
           event.stopPropagation();
           form.classList.add("was-validated");
-          return; 
+          return;
         }
 
         // SweetAlert
@@ -21,6 +21,7 @@
           text: "Pesanan anda telah diterima. Check email anda untuk melanjutkan pembayaran.",
           icon: "success",
           confirmButtonText: "OK",
+          confirmButtonColor: "#106ebe",
         }).then(() => {
           form.reset();
           form.classList.remove("was-validated");
